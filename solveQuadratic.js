c9b58a99bed1b0f25cc2acca7dd124bf00b6199e
Your Name <anhlee.2013@gmail.com>
2022-04-07 10:05:32 +0700

    complete

--- solveQuadratic.js
+++ solveQuadratic.js
@@ -1,16 +1,21 @@
 function solveQuadratic(a,b,c) {
+    let x1, x2;
     console.log(`Quadratic ax2 + bx + c = 0 with a=${a}, b=${b}, c=${c} has result:`);
     if (a==0) {
         if (b==0) {
-            if (c==0) return console.log(`Infinitely many solutions`);
-            else return console.log(`No solution`);
-        } else return console.log(`Solution x = ${-c/b}`);
+            if (c==0) return `Infinitely many solutions`;
+            else return `No solution`;
+        } else return x1 = -c/b;
     }
     else {
         let delta = b*b - 4*a*c;
-        if (delta<0) return console.log(`No solution`);
-        else if (delta ==0) return console.log(`Double solution x1=x2=${-b/(2*a)}`);
-        else return console.log(`2 solution: x1=${(-b+Math.sqrt(delta))/(2*a)} and x2=${(-b-Math.sqrt(delta))/(2*a)}`);
+        if (delta<0) return 'No solution';
+        else if (delta ==0) return x1=-b/(2*a);
+        else {
+            x1=(-b+Math.sqrt(delta))/(2*a);
+            x2=(-b-Math.sqrt(delta))/(2*a);
+            return [x1,x2];
+        } 
     }
 }
-solveQuadratic(8,12,-20);
+console.log(solveQuadratic(8,12,-20));
